5d3016bff773694db47defff77376cc6da0a2c95
Marketa <madragoun@gmail.com>
2021-11-08 18:35:25 +0100

    added updates & latestUpdates

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,21 @@
-module.exports = () => {
+module.exports = (config) => {
+    // Returns a collection of all updates
+    config.addCollection("updates", (collection) => {
+        return [
+            ...collection.getFilteredByGlob("./src/updates/*.md").reverse(),
+        ];
+    });
+
+    // Returns a collection of the 3 latest updates sorted from newest to oldest
+    config.addCollection("latestUpdates", (collection) => {
+        return [
+            ...collection
+                .getFilteredByGlob("./src/updates/*.md")
+                .reverse()
+                .slice(0, 3),
+        ];
+    });
+
     return {
         markdownTemplateEngine: "njk",
         dataTemplateEngine: "njk",
---
+++ src/novinky.md
@@ -0,0 +1,4 @@
+---
+title: "Novinky"
+layout: "layouts/updates.html"
+---
